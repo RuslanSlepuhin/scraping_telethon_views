e91fc5ac6b2d8516c564d2189ae35d99bdcc8bdc
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-10-20 14:37:52 +0300

    'cors'

--- endpoints/settings.py
+++ endpoints/settings.py
@@ -54,12 +54,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:8080',
-#     'http://127.0.0.1:8000',
-#     'http://localhost:3000',
-# ]
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8000',
+    'http://localhost:3000'
+]
 
 
 ROOT_URLCONF = 'endpoints.urls'
