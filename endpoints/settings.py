2bd6cc4853e6781994a4f3386dab03291514b183
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-10-20 14:50:27 +0300

    'cors'

--- endpoints/settings.py
+++ endpoints/settings.py
@@ -38,17 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'rest_framework',
+    'corsheaders',
     'parser',
     # 'django.contrib.staticfiles_dirs',
     'whitenoise.runserver_nostatic',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # cors
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -56,11 +56,8 @@ MIDDLEWARE = [
 ]
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8000',
-    'http://localhost:3000'
+    "http://localhost:3000",
 ]
-
 
 ROOT_URLCONF = 'endpoints.urls'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
